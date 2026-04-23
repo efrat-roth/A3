a22817a45d41adb9062139e1e39df18e7bf72d59
efrat <efratroth15@gmail.com>
2026-04-23 12:01:57 +0300

    Add class of storage of the data

--- A3/src/main/java/storage/IndexStorage.java
+++ A3/src/main/java/storage/IndexStorage.java
@@ -1,32 +1,49 @@
-package DocumentStore;
+package storage;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import storage.InvertedIndex.InvertedIndex;
 
 import java.util.HashMap;
 import java.util.Map;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
-public class DocumentFactory {
+@RequiredArgsConstructor
+public class IndexStorage {
     @Getter
-    private Map<String,Document> documents;
+    private final Map<String, HashMap<Field, String>> documents = new HashMap<>();
+    @Getter
+    private final InvertedIndex invertedIndex;
 
-    public void addDocument(Document document)
-    {
-        if (this.documents == null) {
+    public void addDocument(String documentId, HashMap<Field, String> document) {
+        if (document == null) {
             log.warn("Document has not been initialized: null pointer");
+            return;
+        } else if (this.documents.containsKey(documentId)) {
+            log.warn("Document has already been initialized: id {}", documentId);
         }
-        else if (this.documents.containsKey(document.getId())) {
-            log.warn("Document has already been initialized: id " + document.getId());
+        int docLength = document.keySet().stream().map(Field::getLength).mapToInt(Integer::intValue).sum();
+        //add to document
+        documents.put(documentId, new HashMap<>(document.entrySet().stream()
+                .filter(item -> item.getKey().isStored())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))));
+        //add each term to the invertedIndex
+        Stream<Map.Entry<Field, String>> indexedFields = document.entrySet().stream()
+                .filter(field -> field.getKey().isIndexed());
+        indexedFields.forEach(field -> field.getKey().getValues().forEach((key, value1) ->
+                invertedIndex.addField(field.getKey().getFieldName(), key, documentId, value1, docLength)));
+
+    }
+
+    public HashMap<Field, String> getDocument(String documentId) {
+        if (this.documents.containsKey(documentId)) {
+            return this.documents.get(documentId);
         }
-        else
-        {
-            if (documents == null) {
-                documents = new HashMap<>();
-            }
-            documents.put(document.getId(), document)
-        }
+        log.warn("Document not found: id {}", documentId);
+        //add exception
+        return null;
     }
 }
