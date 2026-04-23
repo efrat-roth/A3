bb244e6b986a9e8f5159276e9ca05acf80865a6f
efrat <efratroth15@gmail.com>
2026-04-23 12:00:48 +0300

    Add class of docId with data of the term

--- A3/src/main/java/storage/InvertedIndex/PostingList.java
+++ A3/src/main/java/storage/InvertedIndex/PostingList.java
@@ -1,4 +1,13 @@
 package storage.InvertedIndex;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public class PostingList {
+    @Getter
+    private Map<String, TermStats> postings = new HashMap<>();
 }
