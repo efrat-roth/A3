ce7a6654b510fe4b2013a36fc263489acc96d711
efrat <efratroth15@gmail.com>
2026-04-23 12:02:28 +0300

    Add class of field, using as a DTO

--- A3/src/main/java/storage/Field.java
+++ A3/src/main/java/storage/Field.java
@@ -1,4 +1,21 @@
 package storage;
 
+import  lombok.Data;
+import lombok.NonNull;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+@Data
 public class Field {
+    @NonNull
+    private String fieldName;
+    @NonNull
+    private Type fieldType;
+    private int length;
+    private boolean stored;
+    private boolean indexed;
+    //term and position
+    private final Map<String, Integer> values;
 }
