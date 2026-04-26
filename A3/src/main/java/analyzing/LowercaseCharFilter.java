9148b8ec299b2089a3705b6e44aeb3bd15dd3789
efrat <efratroth15@gmail.com>
2026-04-26 10:06:15 +0300

    feat: Add an implementation of charfilter - lowercase

--- A3/src/main/java/analyzing/LowercaseCharFilter.java
+++ A3/src/main/java/analyzing/LowercaseCharFilter.java
@@ -1,4 +1,12 @@
 package analyzing;
+//For thinking - which type will be the return value, what design pattern is the solution? decorator, builder...
+//anlyzer.charfilters.stream.forEach
 
-public class LowercaseCharFilter {
+public class LowercaseCharFilter implements CharFilter {
+
+    @Override
+    public String apply(String input) {
+        input = input.toLowerCase();
+        return input;
+    }
 }
