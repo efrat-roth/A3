be5ab7e5f26260d10d81bf80f3be64f72d59207a
efrat <efratroth15@gmail.com>
2026-04-26 10:05:38 +0300

    feat: Add an interface of token filter

--- A3/src/main/java/analyzing/TokenFilter.java
+++ A3/src/main/java/analyzing/TokenFilter.java
@@ -1,4 +1,12 @@
 package analyzing;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public interface TokenFilter {
+
+    List<Token> apply(List<Token> tokens);
 }
