c7624b4ad93821ca366ed58db43402015bfc038e
efrat <efratroth15@gmail.com>
2026-04-26 10:05:26 +0300

    feat: Add an interface of tokenizer

--- A3/src/main/java/analyzing/Tokenizer.java
+++ A3/src/main/java/analyzing/Tokenizer.java
@@ -1,4 +1,11 @@
 package analyzing;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public interface Tokenizer {
+    List<Token> tokenize(String input);
 }
