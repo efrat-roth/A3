447b88d7493366527a68c2936475884e11a1f68b
efrat <efratroth15@gmail.com>
2026-04-26 10:07:11 +0300

    feat: Add an implementation of charfilter - punctuation

--- A3/src/main/java/analyzing/PunctuationCharFilter.java
+++ A3/src/main/java/analyzing/PunctuationCharFilter.java
@@ -1,4 +1,22 @@
 package analyzing;
 
-public class PunctuationCharFilter {
+import lombok.AllArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+public class PunctuationCharFilter implements CharFilter {
+    Set<Character> punctuations = new HashSet<Character>();
+
+    @Override
+    public String apply(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (!punctuations.contains(c)) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 }
