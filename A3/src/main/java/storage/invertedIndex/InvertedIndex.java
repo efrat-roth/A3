72c7c2f5e3cfb1e0a4eb8636b09df13b2091373b
efrat <efratroth15@gmail.com>
2026-04-27 09:43:45 +0300

    Fix: Change the inverted index to interface, and ass an implementation to in memory inevrted index

--- A3/src/main/java/storage/invertedIndex/InvertedIndex.java
+++ A3/src/main/java/storage/invertedIndex/InvertedIndex.java
@@ -9,45 +9,14 @@ import java.util.List;
 import java.util.Map;
 
 @Slf4j
-public class InvertedIndex {
+public interface InvertedIndex {
     //String: FieldName, String2: term, String3:docId
-    @Getter
-    private Map<String, Map<String, PostingList>> index = new HashMap<>();
 
-    public void addField(String fieldName, String token, String docId, int position, int docLength) {
-        Map<String, PostingList> result = index.putIfAbsent(fieldName, Map.of(token, new PostingList(
-                Map.of(docId, new TermStats(1.0 / docLength, new ArrayList<>(List.of(position)))))));
-        if (result == null)
-            addTerm(fieldName, token, docId, position, docLength);
+    public void addField(String fieldName, String token, String docId, int position, int docLength);
 
-    }
+    public void addTerm(String fieldName, String token, String docId, int position, int docLength) ;
 
-    public void addTerm(String fieldName, String token, String docId, int position, int docLength) {
-        if (!index.containsKey(fieldName))
-            addField(fieldName, token, docId, position, docLength);
-        Map<String, PostingList> fieldEntry = index.get(fieldName);
-        //Not has token already
-        if (!fieldEntry.containsKey(token)) {
-            fieldEntry.put(token, new PostingList(Map.of(docId, new TermStats(1, List.of(position)))));
-        } else {
-            addDoc(fieldEntry, token, docId, position, docLength);
-        }
-
-    }
-
-    private void addDoc(Map<String, PostingList> fieldEntry, String token, String docId, int position, int docLength) {
-        Map<String, TermStats> tokenEntry = fieldEntry.get(token).getPostings();
-        //if doc is already exist
-        //improve time running with the list - for thinking if sorted list is needed
-        if (tokenEntry.containsKey(docId)) {
-            tokenEntry.get(docId).incrementTf(1.0 / docLength);
-            tokenEntry.get(docId).getPositions().add(position);
-        } else
-            tokenEntry.put(docId, new TermStats(1.0 / docLength, List.of(position)));
-    }
-    public PostingList getPostingList(String fieldName, String term){
-        return index.get(fieldName).get(term);
-    }
+    public PostingList getPostingList(String fieldName, String term);
 }
 
 
