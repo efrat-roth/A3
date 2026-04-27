933bb6f4fc648cfb4b86122d36ee704aaaee10d8
efrat <efratroth15@gmail.com>
2026-04-27 11:52:52 +0300

    Feat: Add query context class as DTO for score logic

--- A3/src/main/java/reading/QueryContext.java
+++ A3/src/main/java/reading/QueryContext.java
@@ -1,4 +1,15 @@
 package reading;
 
+import lombok.Data;
+import storage.invertedIndex.PostingList;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
 public class QueryContext {
+    private List<String> terms;
+    private Map<String, PostingList> stats;
+    private int totalDocs;
+    private Map<String, Integer> docFreq;
 }
