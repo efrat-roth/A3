c9e53ed50ca74c3bbf696a18fcfd3b7cda7c9b00
efrat <efratroth15@gmail.com>
2026-04-27 11:52:19 +0300

    Feat: Add utils package

--- A3/src/main/java/utils/ConfigManager.java
+++ A3/src/main/java/utils/ConfigManager.java
@@ -1,26 +1,22 @@
 package utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStream;
-import java.util.Properties;
 
-public class ConfigurationManager {
-    public static Properties load(String fileName, String propertiesName) {
-        Properties props = new Properties();
+public class ConfigManager {
 
-        try (InputStream input =
-                     ConfigurationManager.class.getClassLoader()
-                             .getResourceAsStream(fileName)) {
+    public static void load(String fileName, String propertyName) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
 
-            if (input == null) {
-                throw new IllegalArgumentException("File not found: " + fileName);
-            }
+            InputStream input =
+                    ConfigLoader.class.getClassLoader()
+                            .getResourceAsStream(fileName);
 
-            props.load(input);
+            return mapper.readValue(input, AnalyzerConfig.class);
 
         } catch (Exception e) {
-            throw new RuntimeException("Failed to load properties file", e);
+            throw new RuntimeException("Failed to load config", e);
         }
-
-        return props;
     }
 }
--- A3/src/main/java/utils/FileReader.java
+++ A3/src/main/java/utils/FileReader.java
@@ -1,4 +1,14 @@
 package utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class FileReader {
+    public List<String> readFileLines(String fileName) throws IOException {
+        //קריאה מקובץ קונפיגורציה של הpath לפי שם הקובץ
+        return Files.readAllLines(Path.of(fileName));
+
+    }
 }
