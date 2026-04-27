1695a00d0d22379731b6bdc9724883e43a9cf421
efrat <efratroth15@gmail.com>
2026-04-27 10:59:19 +0300

    Feat: Implement the in memory indexing- adding a document

--- A3/src/main/java/indexing/IndexWriter.java
+++ A3/src/main/java/indexing/IndexWriter.java
@@ -1,4 +1,11 @@
 package indexing;
 
+import storage.Field;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public interface IndexWriter {
+    public void addDocument(List<Field> document);
 }
