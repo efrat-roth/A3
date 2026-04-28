4b2f11877e5660718e2ecb1c2ebd0eb29136b753
efrat <efratroth15@gmail.com>
2026-04-28 15:10:32 +0300

    Feat: Add interface for calc score

--- A3/src/main/java/scoring/calculation/ScoreCalculator.java
+++ A3/src/main/java/scoring/calculation/ScoreCalculator.java
@@ -6,5 +6,5 @@ import scoring.ScoreResult;
 import java.util.List;
 
 public interface ScoreCalculator {
-    List<ScoreResult> calculateScores(QueryContext queryContext);
+   ScoreResult calculateScores(QueryContext queryContext);
 }
