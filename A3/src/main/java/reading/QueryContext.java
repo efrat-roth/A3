f5b1ad1d1b3c6bd7756b906735c7d1146799a85b
efrat <efratroth15@gmail.com>
2026-04-28 14:46:27 +0300

    Fix: Change the fields to match the tfIdf needings

--- A3/src/main/java/reading/QueryContext.java
+++ A3/src/main/java/reading/QueryContext.java
@@ -2,14 +2,15 @@ package reading;
 
 import lombok.Data;
 import storage.invertedIndex.PostingList;
+import storage.invertedIndex.TermStats;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class QueryContext {
-    private final List<String> terms;
-    private final Map<String, PostingList> stats;
-    private final int totalDocs;
-    private final Map<String, Integer> docFreq;
+    private final String docId;
+    private final List<TermStats> statsOfDoc;
+    private final Map<String, Integer> termsDf;
+    private final int docsCount;
 }
