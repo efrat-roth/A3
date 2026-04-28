257fd2d2391f846beef38eb623444a4cef3c0b6a
efrat <efratroth15@gmail.com>
2026-04-28 14:50:28 +0300

    Feat: Add a class of query

--- A3/src/main/java/quering/Query.java
+++ A3/src/main/java/quering/Query.java
@@ -1,4 +1,15 @@
 package quering;
 
+import lombok.Data;
+
+import java.util.Map;
+
+
+@Data
 public class Query {
+    private final String queryId;
+    private final Map<String, String> conditions;
+    private final int limit;
+    private final int start;
+    private final QueryType queryType;
 }
