b1a912aac075f64ba3f7ce450aacbae4680de5c9
efrat <efratroth15@gmail.com>
2026-04-29 09:31:30 +0300

    Feat: add a class of definition the type of analyzer in configuration: AnalyzerDefinition

--- A3/src/main/java/utils/config/AnalyzerDefinition.java
+++ A3/src/main/java/utils/config/AnalyzerDefinition.java
@@ -1,4 +1,12 @@
 package utils.config;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class AnalyzerDefinition {
+    private List<String> charFilters;
+    private String tokenizer;
+    private List<String> tokenFilters;
 }
