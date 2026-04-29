7cf49f7e6d340b8fdb7e9cc7a23cc7b8a4975de0
efrat <efratroth15@gmail.com>
2026-04-29 10:59:50 +0300

    Add analyzerStrategy factory class

--- A3/src/main/java/analyzing/analyzerStrategy/DefaultAnalyzerStrategy.java
+++ A3/src/main/java/analyzing/analyzerStrategy/DefaultAnalyzerStrategy.java
@@ -2,15 +2,16 @@ package analyzing.analyzerStrategy;
 
 import analyzing.Analyzer;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import storage.Field;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class DefaultAnalyzerStrategy implements AnalyzerStrategy {
     @Getter
-    private final Analyzer analyzer;
+    private Analyzer analyzer;
 
     @Override
     public Analyzer getAnalyzer(String field) {
--- A3/src/main/java/analyzing/analyzerStrategy/FieldBasedAnalyzerStrategy.java
+++ A3/src/main/java/analyzing/analyzerStrategy/FieldBasedAnalyzerStrategy.java
@@ -2,16 +2,17 @@ package analyzing.analyzerStrategy;
 
 import analyzing.Analyzer;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import storage.Field;
 
 import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class FieldBasedAnalyzerStrategy implements AnalyzerStrategy {
     @Getter
-    private final Map<String, Analyzer> analyzers;
+    private Map<String, Analyzer> analyzers;
 
     @Override
     public Analyzer getAnalyzer(String field) {
