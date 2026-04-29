276128c3f81e39b00d27134c43555c773841fdab
efrat <efratroth15@gmail.com>
2026-04-29 09:24:02 +0300

    Feat: Add a class of loading a configuration file: config loader

--- A3/src/main/java/utils/ConfigLoader.java
+++ A3/src/main/java/utils/ConfigLoader.java
@@ -1,14 +1,15 @@
 package utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
 
-public class AppConfig {
+public class ConfigLoader {
 
-    public static AppConfig load(String path) throws IOException {
+    public static ConfigLoader load(String path) throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        return mapper.readValue(new File(path), AppConfig.class);
+        return mapper.readValue(new File(path), ConfigLoader.class);
     }
 }
