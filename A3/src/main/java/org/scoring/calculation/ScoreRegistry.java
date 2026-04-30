5b17295e0cb4e1e1df107490efbd8d2cee4a6fe6
efrat <efratroth15@gmail.com>
2026-04-30 08:46:51 +0300

    Feat: add logs to the package scoring

--- A3/src/main/java/org/scoring/calculation/ScoreRegistry.java
+++ A3/src/main/java/org/scoring/calculation/ScoreRegistry.java
@@ -1,9 +1,12 @@
 package org.scoring.calculation;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+@Slf4j
 public class ScoreRegistry {
 
     private final Map<String, Supplier<ScoreCalculator>> scorers =
@@ -11,18 +14,22 @@ public class ScoreRegistry {
 
     public ScoreRegistry() {
         scorers.put("tfidf", TfIdfScorer::new);
+        log.debug("Registered scorers: {}", scorers.keySet());
     }
 
     public ScoreCalculator get(String name) {
+        log.debug("Retrieving scorer: {}", name);
 
         Supplier<ScoreCalculator> supplier = scorers.get(name);
 
         if (supplier == null) {
+            log.warn("Unknown scorer requested: {}", name);
             throw new IllegalArgumentException(
                     "Unknown scorer: " + name
             );
         }
 
+        log.debug("Scorer found: {}", name);
         return supplier.get();
     }
 }
--- A3/src/main/java/org/scoring/calculation/TfIdfScorer.java
+++ A3/src/main/java/org/scoring/calculation/TfIdfScorer.java
@@ -1,15 +1,21 @@
 package org.scoring.calculation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reading.QueryContext;
 import org.scoring.ScoreResult;
 import org.storage.invertedIndex.TermStats;
 
+@Slf4j
 public class TfIdfScorer implements ScoreCalculator {
     @Override
     public ScoreResult calculateScores(QueryContext context) {
+        log.debug("Calculating TF-IDF score: docId {}, termStatsCount {}, termsDfCount {}, docsCount {}",
+                context.getDocId(), context.getStatsOfDoc().size(), context.getTermsDf().size(), context.getDocsCount());
         double tf = context.getStatsOfDoc().stream().mapToDouble(TermStats::getTf).sum();
         double idf = context.getTermsDf().values().stream().mapToDouble(
                 dfOfTerm -> (Math.log(context.getDocsCount() / (double) dfOfTerm))).sum();
-        return new ScoreResult(context.getDocId(), tf * idf);
+        double totalScore = tf * idf;
+        log.debug("TF-IDF score calculated: docId {}, tf {}, idf {}, totalScore {}", context.getDocId(), tf, idf, totalScore);
+        return new ScoreResult(context.getDocId(), totalScore);
     }
 }
