fab59a7c7e603998c351fa71ddfc1debb165c2ed
efrat <efratroth15@gmail.com>
2026-05-06 11:54:26 +0300

    Fix: add logs to invertedIndex factory class

--- A3/src/main/java/org/storage/invertedIndex/InvertedIndexFactory.java
+++ A3/src/main/java/org/storage/invertedIndex/InvertedIndexFactory.java
@@ -1,28 +1,29 @@
 package org.storage.invertedIndex;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.utils.Exceptions;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+@Slf4j
 public class InvertedIndexFactory {
 
     private final Map<String, Supplier<InvertedIndex>> registry = new HashMap<>();
 
     public InvertedIndexFactory() {
         registry.put("inMemory", InMemoryInvertedIndex::new);
+        log.debug("Registered memory types: {}", registry.keySet());
     }
 
     public InvertedIndex create(String type) {
-        if (type == null || type.isBlank()) {
-            throw new Exceptions.InvalidIndexEntryException("Inverted index type cannot be null or blank");
-        }
-
-        Supplier<InvertedIndex> supplier = registry.get(type);
+        log.debug("Retrieving memory type {}", type);
+       Supplier<InvertedIndex> supplier = registry.get(type);
 
         if (supplier == null) {
+            log.warn("Unknown inverted index requested: {}", type);
             throw new Exceptions.InvalidIndexEntryException("Unknown inverted index type: " + type);
         }
 
