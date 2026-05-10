b56d5ac3f07aeec217fbc511d86a51d1c795d79d
efrat <efratroth15@gmail.com>
2026-05-10 09:12:41 +0300

    Fix: Delete the unneseccery validateContext function - the data is from a validate code processes

--- A3/src/main/java/org/scoring/calculation/TfIdfScorer.java
+++ A3/src/main/java/org/scoring/calculation/TfIdfScorer.java
@@ -12,8 +12,6 @@ public class TfIdfScorer implements ScoreCalculator {
     @Override
     public ScoreResult calculateScores(QueryContext context) {
 
-        validateContext(context);
-
         log.debug("Calculating TF-IDF score: docId {}, TermScoreDTO {}, docsCount {}",
                 context.getDocId(), context.getTermScoreDTO().size(), context.getDocsCount());
         double score = 0.0;
@@ -27,29 +25,5 @@ public class TfIdfScorer implements ScoreCalculator {
 
         return new ScoreResult(context.getDocId(), score);
     }
-
-    private void validateContext(QueryContext context) {
-
-        if (context == null) {
-            throw new Exceptions.InvalidTermStatsException("QueryContext cannot be null");
-        }
-
-        if (context.getDocId() == null || context.getDocId().isBlank()) {
-            throw new Exceptions.DocumentNotFoundException("Document id is missing");
-        }
-
-        if (context.getDocsCount() <= 0) {
-            throw new Exceptions.InvalidIndexEntryException("Documents count must be greater than zero");
-        }
-
-        if (context.getTermScoreDTO() == null || context.getTermScoreDTO().isEmpty()) {
-            throw new Exceptions.TermNotFoundException("No term DTO data available");
-        }
-
-        boolean hasInvalidDf = context.getTermScoreDTO().stream().anyMatch(DTO -> DTO.idf() <= 0);
-
-        if (hasInvalidDf) {
-            throw new Exceptions.InvalidTermStatsException("Document frequency must be greater than zero");
-        }
-    }
+    
 }
