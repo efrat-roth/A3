e075c9df1e5794e15e7a41a5de11645658db232a
efrat <efratroth15@gmail.com>
2026-05-10 08:50:32 +0300

    Fix: Change the object mapper in config loader class to be a singelton

--- A3/src/main/java/org/utils/ConfigLoader.java
+++ A3/src/main/java/org/utils/ConfigLoader.java
@@ -7,14 +7,15 @@ import org.utils.config.AppConfig;
 import java.io.File;
 import java.io.IOException;
 
+
 public class ConfigLoader {
+    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
     public static AppConfig load() throws IOException {
         return load("src/main/resources/application.yml");
     }
 
     public static AppConfig load(String path) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         return mapper.readValue(new File(path), AppConfig.class);
     }
 }
