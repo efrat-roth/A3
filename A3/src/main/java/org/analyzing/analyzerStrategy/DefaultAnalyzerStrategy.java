124f1bf8720db062cbe0941e5418342aa6cc2610
efrat <efratroth15@gmail.com>
2026-05-19 08:29:06 +0300

    Fix: the validation checks in analyzers strategy are in the c-tor instead of the get function

--- A3/src/main/java/org/analyzing/analyzerStrategy/DefaultAnalyzerStrategy.java
+++ A3/src/main/java/org/analyzing/analyzerStrategy/DefaultAnalyzerStrategy.java
@@ -1,22 +1,19 @@
 package org.analyzing.analyzerStrategy;
 
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.analyzing.Analyzer;
-import org.utils.Exceptions;
+
 
 @Slf4j
 @AllArgsConstructor
 public class DefaultAnalyzerStrategy implements AnalyzerStrategy {
-
+    @NonNull
     private final Analyzer analyzer;
 
     @Override
     public Analyzer getAnalyzer(String field) {
-
-        if (analyzer == null) {
-            throw new Exceptions.AnalyzerConfigurationException("Default analyzer is not configured");
-        }
         return analyzer;
     }
 }
--- A3/src/main/java/org/analyzing/analyzerStrategy/FieldBasedAnalyzerStrategy.java
+++ A3/src/main/java/org/analyzing/analyzerStrategy/FieldBasedAnalyzerStrategy.java
@@ -1,6 +1,6 @@
 package org.analyzing.analyzerStrategy;
 
-import lombok.Getter;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.analyzing.Analyzer;
 import org.utils.Exceptions;
@@ -9,24 +9,18 @@ import java.util.Map;
 
 @Slf4j
 public class FieldBasedAnalyzerStrategy implements AnalyzerStrategy {
-    @Getter
+    @NonNull
     private final Map<String, Analyzer> analyzers;
 
     public FieldBasedAnalyzerStrategy(Map<String, Analyzer> analyzers) {
+        if (analyzers.containsValue(null) || analyzers.containsKey(null)) {
+            throw new Exceptions.AnalyzerNotFoundException("Map has a null value");
+        }
         this.analyzers = analyzers;
     }
 
     @Override
     public Analyzer getAnalyzer(String field) {
-
-        if (analyzers == null || analyzers.isEmpty()) {
-            throw new Exceptions.AnalyzerConfigurationException("Field analyzers are not configured");
-        }
-
-        if (analyzers.get(field) == null) {
-            throw new Exceptions.AnalyzerNotFoundException("No analyzer configured for field: " + field);
-        }
-
         return analyzers.get(field);
     }
 }
